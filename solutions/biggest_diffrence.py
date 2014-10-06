a094ec752f7378b05ff96e7b9e3b10bb8513f43e
alexnad <alexandernadjarian@gmail.com>
2014-10-06 23:50:23 +0300

    Changed biggest_difference.py

--- solutions/biggest_diffrence.py
+++ solutions/biggest_diffrence.py
@@ -4,7 +4,7 @@ def biggest_difference(arr):
     for num1 in arr:
         a = int(num1)
         for num2 in arr[begin:]:
-            a = int(num2)
+            b = int(num2)
             begin += 1
             if a - b < minimum or b - a < minimum:
                 minimum = -abs(a - b)
