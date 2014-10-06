b224a35773aec77949c3fcb252969e6e645d1903
alexnad <alexandernadjarian@gmail.com>
2014-10-06 23:45:25 +0300

    Add is_increasing.py

---
+++ solutions/is_increasing.py
@@ -0,0 +1,8 @@
+def is_increasing(seq):
+    previous_element = seq[0]
+    for next_element in seq[1:]:
+        if previous_element >= next_element:
+            return False
+        previous_element = next_element
+
+    return True
