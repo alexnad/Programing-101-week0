f57d1f860e4552cbe5d77ef2e97b70a9cd2d7971
alexnad <alexandernadjarian@gmail.com>
2014-10-10 18:46:46 +0300

    "add changes"

--- solutions/magic_square.py
+++ solutions/magic_square.py
@@ -1,6 +1,9 @@
+
+
 def magic_square(matrix):
     transponed = list(map(list, zip(*matrix)))
 
-    sum_rows = 0
+    square_sum = sum(matrix[0])
     for rows in matrix:
-        for number in rows
+        if sum != sum_of_rows:
+            return False
