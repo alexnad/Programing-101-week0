837c336229884d375cc9a0e92f04f87ecea6e2df
Alexander Nadjarian <alexandernadjarian@gmail.com>
2014-10-12 15:47:52 +0300

    Update matrix_bombing_plan.py

--- solutions/matrix_bombing_plan.py
+++ solutions/matrix_bombing_plan.py
@@ -1,6 +1,6 @@
 def valid_index(matrix, index, bomb_place):
     valid_row_index = index[0] >= 0 and index[0] < len(matrix)
-    valid_column_index = index[1] >= 0 and index[1] < len(matrix)
+    valid_column_index = index[1] >= 0 and index[1] < len(matrix[0])
 
     return valid_column_index and valid_row_index and index != bomb_place
 
@@ -12,8 +12,8 @@ def detonate(element, amount):
 
 
 def plant_bomb(matrix, index):
-    bomb_damage = matrix[index[0]][index[1]]
-
+    bomb_damage = matrix[index[0]][index[1]]    
+    
     for i in range(index[0] - 1, index[0] + 2):
         for j in range(index[1] - 1, index[1] + 2):
             if valid_index(matrix, (i, j), index):
